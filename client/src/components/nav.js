1da847c4c1e9358fa8e95f38bf6114843342ba8f
nicholas llerandi <nicholasllerandi@gmail.com>
2019-01-12 10:37:48 -0500

    her we go

---
+++ client/src/components/nav.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div className="Nav">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
+                    <a className="nav-item nav-link" href="#">Features</a>
+                    <a className="nav-item nav-link" href="#">Pricing</a>
+                </div>
+                </div>
+                <form className="form-inline my-2 my-lg-0">
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </nav>
+        </div>
+    );
+};
+
+export default Nav
