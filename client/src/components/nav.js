355bb3d89a6989863ed9313b968da797a49a3b1a
nicholas llerandi <nicholasllerandi@gmail.com>
2019-01-14 21:11:32 -0500

    gitignore edits

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,26 +1,39 @@
 import React from 'react';
 
+import './nav.scss'
+
 const Nav = () => {
     return (
-        <div className="Nav">
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
-                    <a className="nav-item nav-link" href="#">Features</a>
-                    <a className="nav-item nav-link" href="#">Pricing</a>
-                </div>
-                </div>
-                <form className="form-inline my-2 my-lg-0">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+
+            <a className="navbar-brand" href="#">Fullstack Musician</a>
+
+            <button className="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span
+                className="navbar-toggler-icon"></span></button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
+
+                <form className="form-inline active-cyan-4">
+                    <input className="form-control form-control-sm mr-3 w-75" type="text" placeholder="Search"
+                           aria-label="Search"/>
                 </form>
-            </nav>
-        </div>
+
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Login</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Signup</a>
+                    </li>
+
+
+                </ul>
+            </div>
+
+        </nav>
     );
 };
 
