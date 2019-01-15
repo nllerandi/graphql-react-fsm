cedce3117551562b05650d1c4deb0c90e4190062
nicholas llerandi <nicholasllerandi@gmail.com>
2019-01-14 21:08:54 -0500

    nav styles

---
+++ server/demoData.js
@@ -0,0 +1,89 @@
+const demoQuestions = [{
+        "id": 1,
+        "title": "How do I install Cubase?",
+        "asker": "123",
+        "date": "DATE ASKED",
+        "answers": 10,
+        "tags": [
+            "Cubase",
+            "Production",
+            "Mastering"
+        ]
+    },
+    {
+        "id": 2,
+        "title": "What does this error mean?",
+        "asker": "456",
+        "date": "DATE ASKED",
+        "answers": 4,
+        "tags": [
+            "ProTools",
+            "Production"
+        ]
+    },
+    {
+        "id": 3,
+        "title": "Where do I plug this cable in?",
+        "asker": "789",
+        "date": "DATE ASKED",
+        "answers": 7,
+        "tags": [
+            "Hardware",
+            "Production"
+        ]
+    },
+    {
+        "id": 4,
+        "title": "When is the update for this plugin?",
+        "asker": "456",
+        "date": "DATE ASKED",
+        "answers":12,
+        "tags": [
+            "Plugins",
+            "Software"
+        ]
+    },
+    {
+        "id": 5,
+        "title": "Why am I hearing this noise?",
+        "asker": "123",
+        "date": "DATE ASKED",
+        "answers": 45,
+        "tags": [
+            "Pickups",
+            "Guitar",
+            "Gear"
+        ]
+    },
+    {
+        "id": 6,
+        "title": "Headphones don't work",
+        "asker": "456",
+        "date": "DATE ASKED",
+        "answers": 5,
+        "tags": [
+            "Headphones",
+            "Gear"
+        ]
+    }
+]
+
+const demoUsers = [
+    {
+        userId: "123",
+        name: "Nick"
+    },
+    {
+        userId: "456",
+        name: "Leora"
+    },
+    {
+        userId: "789",
+        name: "Peaches"
+    }
+]
+
+export {
+    demoQuestions,
+    demoUsers
+}
